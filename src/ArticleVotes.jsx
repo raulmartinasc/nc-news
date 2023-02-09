39bbdd4522e50f49d3b85eeeb0629e00a4b584e3
raulmartinasc <raulmartinascanio@gmail.com>
2023-02-09 22:33:31 +0000

    Articles Votes Finished

--- src/ArticleVotes.jsx
+++ src/ArticleVotes.jsx
@@ -1,16 +1,40 @@
-import { fetchComments } from "./api";
 import { useState, useEffect } from "react";
+import { fetchArticlesById, patchVotes } from "./api";
 const ArticleVotes = ({ article_id }) => {
   const [votes, setVotes] = useState(0);
+  const [err, setErr] = useState(null);
   useEffect(() => {
-    fetchComments(article_id).then(({ data: { comments } }) => {
-      console.log(comments.votes);
-      setVotes(comments.votes);
+    fetchArticlesById(article_id).then(({ data: { article } }) => {
+      setVotes(article.votes);
     });
   }, [article_id]);
+
+  const handleVotesClick = (increment) => {
+    setVotes((currentCount) => (currentCount += increment));
+    setErr(null);
+    patchVotes(increment, article_id).catch((err) => {
+      setVotes((currentCount) => currentCount - increment);
+      setErr("Something went wrong, please try again.");
+    });
+  };
   return (
     <section>
-      <h4>{votes}</h4>
+      <h4>Votes: {votes}</h4>
+      {err ? <p>{err}</p> : null}
+      <button
+        onClick={() => {
+          handleVotesClick(1);
+        }}
+      >
+        Upvote
+      </button>
+      <button
+        onClick={() => {
+          handleVotesClick(-1);
+        }}
+      >
+        Downvote
+      </button>
     </section>
   );
 };
--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,17 +1,4 @@
-import { useState } from "react";
-import axios from "axios";
-const Comments = ({ comments, article_id }) => {
-  const [votes, setVotes] = useState(0);
-  const handleClick = (increment) => {
-    axios
-      .patch(
-        `https://nc-news-api-mq3o.onrender.com/api/articles/${article_id}`,
-        { inc_votes: increment }
-      )
-      .then(({ data }) => {
-        console.log(data);
-      });
-  };
+const Comments = ({ comments }) => {
   return (
     <section>
       <h1>Comments</h1>
@@ -21,9 +8,6 @@ const Comments = ({ comments, article_id }) => {
             <li key={comment.comment_id}>
               <h4>{comment.author}: </h4>
               <p>{comment.body}</p>
-              <h4>Votes: {comment.votes}</h4>
-              <button onClick={handleClick(1)}>UpVote</button>
-              <button onClick={handleClick(-1)}>DownVote</button>
             </li>
           );
         })}
